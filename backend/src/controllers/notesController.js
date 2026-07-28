3ac755bf1df379f40455027446049f460bcbc927
Manuel Alejandro Romero Chavarro <manuelromero.im@gmail.com>
2026-07-28 16:02:48 -0500

    first getAllNotes controller work

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,5 +1,12 @@
-export function getAllNotes(req, res) {
-  res.status(200).send("You just fetched the notes")
+import Note from "../models/Note.js"
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find()
+    res.status(200).json(notes)
+  } catch (error) {
+    console.error("Error in getAllnotes Controller", error)
+    res.status(500).json({ message: "Internal server error" })
+  }
 }
 
 export function createNote(req, res) {
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+
 connectDB()
 
 // notes routes
