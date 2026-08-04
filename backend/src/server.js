ba9cdd695b24f0277495ab66043a2b0af1e9ff54
Manuel Alejandro Romero Chavarro <manuelromero.im@gmail.com>
2026-08-03 19:13:43 -0500

    add HomePage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
-import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js"
 
 dotenv.config()
@@ -12,13 +14,15 @@ const PORT = process.env.PORT || 5001
 
 
 // middleware
+app.use(cors({
+  origin: "http://localhost:5173",
+}))
 app.use(express.json()) // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter)
+
+
 // our siemple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} and URL is ${req.url}`)
-//   next()
-// })
+// app.use()
 
 // notes routes
 app.use("/api/notes", notesRoutes)
